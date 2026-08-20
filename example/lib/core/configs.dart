834513c416e7f15e24bb4e0aacd774befa673aa8
Farhad <farhad@toomba.nl>
2026-08-20 21:36:37 +0200

    Developed an enviroment parser and remove dotenv package

--- example/lib/core/configs.dart
+++ example/lib/core/configs.dart
@@ -6,40 +6,40 @@ import 'package:finch/finch_model.dart';
 import 'package:finch/finch_tools.dart';
 
 FinchConfigs configs = FinchConfigs(
-  pathCache: pathTo(env['PATH_CACHE'] ?? './cache_routes'),
+  pathCache: pathTo(env.get('PATH_CACHE', './cache_routes')),
   jinjaMapTemplate: mapTemplates,
-  widgetsPath: pathTo(env['WIDGETS_PATH'] ?? "./lib/widgets"),
-  widgetsType: env['WIDGETS_TYPE'] ?? 'j2.html',
-  languagePath: pathTo(env['LANGUAGE_PATH'] ?? "./lib/languages"),
+  widgetsPath: pathTo(env.get('WIDGETS_PATH', "./lib/widgets")),
+  widgetsType: env.get('WIDGETS_TYPE', 'j2.html'),
+  languagePath: pathTo(env.get('LANGUAGE_PATH', "./lib/languages")),
   languageSource: LanguageSource.dart,
   dartLanguages: languageDart,
-  publicDir: pathTo(env['PUBLIC_DIR'] ?? './public'),
+  publicDir: pathTo(env.get('PUBLIC_DIR', './public')),
   dbConfig: FinchDBConfig(
-    enable: true, //env['ENABLE_DATABASE'] == 'true',
-    dbName: env['MONGODB_NAME'] ?? 'example',
-    auth: env['MONGODB_AUTH'] ?? 'admin',
-    pass: env['MONGODB_PASSWORD'] ?? 'PasswordMongoDB',
-    host: env['MONGODB_CONNECTION'] ?? 'localhost',
-    port: env['MONGODB_PORT'] ?? '27018',
-    user: env['MONGODB_USER'] ?? 'root',
+    enable: true, //env.get('ENABLE_DATABASE', 'true') == 'true',
+    dbName: env.get('MONGODB_NAME', 'example'),
+    auth: env.get('MONGODB_AUTH', 'admin'),
+    pass: env.get('MONGODB_PASSWORD', 'PasswordMongoDB'),
+    host: env.get('MONGODB_CONNECTION', 'localhost'),
+    port: env.get('MONGODB_PORT', '27018'),
+    user: env.get('MONGODB_USER', 'root'),
   ),
-  port: (env['DOMAIN_PORT'] ?? '8085').toInt(def: 8085),
+  port: (env.getInt('DOMAIN_PORT', 8085)),
   mysqlConfig: FinchMysqlConfig(
     enable: true,
-    host: env['MYSQL_HOST'] ?? 'localhost',
-    port: (env['MYSQL_PORT'] ?? '3306').toInt(def: 3306),
-    user: env['MYSQL_USER'] ?? 'example_user',
-    pass: env['MYSQL_PASSWORD'] ?? 'example_password',
-    databaseName: env['MYSQL_DATABASE'] ?? 'example_db',
+    host: env.get('MYSQL_HOST', 'localhost'),
+    port: (env.getInt('MYSQL_PORT', 3306)),
+    user: env.get('MYSQL_USER', 'example_user'),
+    pass: env.get('MYSQL_PASSWORD', 'example_password'),
+    databaseName: env.get('MYSQL_DATABASE', 'example_db'),
   ),
 
   /// Enable local debugger
-  enableLocalDebugger: (env['ENABLE_LOCAL_DEBUGGER'] ?? true).toString().toBool,
+  enableLocalDebugger: (env.getBool('ENABLE_LOCAL_DEBUGGER', true)),
 
   /// SQLite configuration
   sqliteConfig: FinchSqliteConfig(
     enable: true,
-    filePath: env['SQLITE_PATH'] ?? './example_database.sqlite',
+    filePath: env.get('SQLITE_PATH', './example_database.sqlite'),
   ),
   errorWidget: ErrorCustomView(),
 );
